abb016181932b052fe763d54cbf1176b137cc569
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2023-09-22 23:46:52 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
 #include "lists.h"
 
 /**
- * list_len - Calculates the number of elements in a linked list.
- * @h: A pointer to the head of the list.
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
  *
- * Return: The number of elements in the list.
- */
+ *Return: the number of elements
+*/
+
 size_t list_len(const list_t *h)
-{
-    size_t count = 0;
+	{
+		size_t elements = 0;
 
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
+		while (h)
+		{
+			elements++;
+			h = h->next;
+		}
 
-    return count; /* Return the count as an integer */
-}
-
+		return (elements);
+	}
