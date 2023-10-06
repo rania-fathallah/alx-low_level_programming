01aa9a017c081866accaaeebb370d0f538b7ed86
rania-fathallah <87832654+rania-fathallah@users.noreply.github.com>
2023-10-07 00:11:25 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,46 +1,48 @@
 #include "holberton.h"
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX std-output.
- * @filename: the file to open.
- * @letters: number of letters it should read and print.
- * Return: number of letters it could read and print, or 0 if:
- * - filename is NULL.
- * - the file can not be opened or read.
- * - write fails or does not write the expected amount of bytes.
+ * read_textfile - read a text file and print it to stdout
+ * @filename: the name of the file to read
+ * @letters: the number of letters to be read and printed
+ *
+ * Return: If filename is NULL, the file cannot be opened or read, or
+ * write fails or returns an unexpected number of bytes, return 0.
+ * Otherwise, return the actual number of letters read and printed.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor = -1;
-	ssize_t output = 0;
-	char *buffer;
+	char *buffer = NULL;
+	ssize_t b_read;
+	ssize_t b_written;
+	int fd;
 
-	if (!filename)
+	if (!(filename && letters))
 		return (0);
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
 	if (!buffer)
-	{
-		close(file_descriptor);
 		return (0);
-	}
 
-	output = read(file_descriptor, buffer, letters);
-	if (output < 0)
+	b_read = read(fd, buffer, letters);
+	close(fd);
+
+	if (b_read < 0)
 	{
 		free(buffer);
-		close(file_descriptor);
 		return (0);
 	}
+	if (!b_read)
+		b_read = letters;
 
-	output = write(STDOUT_FILENO, buffer, output);
+	b_written = write(STDOUT_FILENO, buffer, b_read);
 	free(buffer);
-	close(file_descriptor);
-	if (output < 0)
+
+	if (b_written < 0)
 		return (0);
-	return (output);
+
+	return (b_written);
 }
